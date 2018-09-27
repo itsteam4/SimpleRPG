d7ec94eea65385390ba5342184e12896a3aadd32
김상용 <triaidize@gmail.com>
2018-09-27 16:17:02 +0900

    제목 : 캐릭터 정보창 구현 1. 작업내용 - 방명록 삭제기능 2.해야될일 - 페이징

--- simplerpg-web/src/main/java/com/github/itsteam4/simplerpg/web/Controller/RankController.java
+++ simplerpg-web/src/main/java/com/github/itsteam4/simplerpg/web/Controller/RankController.java
@@ -23,6 +23,7 @@ import com.github.itsteam4.simplerpg.web.entity.RankTest;
 import com.github.itsteam4.simplerpg.web.entity.RankVisitbook;
 import com.github.itsteam4.simplerpg.web.entity.RankVisitbookNumber;
 import com.github.itsteam4.simplerpg.web.service.RankTestDao;
+import com.github.itsteam4.simplerpg.web.service.FreeBoardDAO;
 import com.github.itsteam4.simplerpg.web.service.MemberDAO;
 import com.github.itsteam4.simplerpg.web.service.RankInfoDao;
 
@@ -39,6 +40,8 @@ public class RankController {
 	private RankVisitbook rankvisitbook;
 	@Autowired
 	Member member;
+	@Autowired
+	private RankVisitbookNumber rankvisitbooknumber;
 	
 	@RequestMapping(value = "/ranktestInsert", method = RequestMethod.POST)
 	public String ranktestInsert(Model model,@ModelAttribute RankTest ranktest) {
@@ -89,6 +92,47 @@ public class RankController {
 		return "rank/dbinsert_form";
 	}
 	
+	@RequestMapping(value = "/rankTest", method = RequestMethod.GET)
+	public String rankTest(Model model,@RequestParam String id,@ModelAttribute Member member,
+			@ModelAttribute RankVisitbook rankvisitbook,@ModelAttribute RankVisitbookNumber rankvisitbooknumber ) {
+		MemberDAO mdao = sqlSession.getMapper(MemberDAO.class);
+		RankTestDao dao = sqlSession.getMapper(RankTestDao.class);
+		RankInfoDao idao = sqlSession.getMapper(RankInfoDao.class);
+		
+		Member members = mdao.selectOne(id);
+		ArrayList<RankVisitbook> rankvisitbooks = idao.selectAll(id); 
+		
+		int number = (int) rankvisitbooks.size();
+		rankvisitbooknumber.setNumber(number);
+		String stanid = id;
+		int rowcount = idao.selectRowCount(stanid);
+		System.out.println("test_id는"+id);
+		System.out.println("test_방명록수"+rowcount);
+		
+		int pagesize = 10;
+		int startrow = 0;
+		int endrow = startrow + pagesize;
+		
+		rankvisitbooknumber.setStartrow(startrow);
+		rankvisitbooknumber.setEndrow(endrow);
+		
+		ArrayList<RankVisitbook> pagebooks = idao.pageList(rankvisitbooknumber);
+		int absPage = 1;
+		if(rowcount % pagesize == 0)
+			absPage = 0;
+		int pageCount = rowcount / pagesize + absPage;
+		int[] pages = new int[pageCount];
+		for(int i = 0; i <pageCount;i++) {
+			pages[i] = i+1;
+		}
+		
+		model.addAttribute("members",members);
+		model.addAttribute("rankvisitbooks",rankvisitbooks);
+		model.addAttribute("rankvisitbooknumber",rankvisitbooknumber);
+		
+		return "rank/rankinfo_form";
+	}
+	
 	
 	@RequestMapping(value = "/rankinfoform", method = RequestMethod.GET)
 	public String rankinfoform(Model model,@RequestParam String id,@ModelAttribute Member member,
@@ -98,17 +142,91 @@ public class RankController {
 		
 		RankInfo rankinfos = dao.selectOne(id);
 		Member members = mdao.selectOne(id);
+		
 		ArrayList<RankVisitbook> rankvisitbooks = dao.selectAll(id);
 		
 		int number = (int) rankvisitbooks.size();
 		rankvisitbooknumber.setNumber(number);
 		
+		int pagesize = 10;
+		int startrow = 0;
+		int endrow = startrow + pagesize;
+		System.out.println("id는"+id);
+		
+		String stanid = id;
+		int rowcount = dao.selectRowCount(stanid);
+		
+		System.out.println("방명록수"+rowcount);
+		
+		rankvisitbooknumber.setStartrow(startrow);
+		rankvisitbooknumber.setEndrow(endrow);
+		
+		
+		ArrayList<RankVisitbook> pagebooks = dao.pageList(rankvisitbooknumber);
+		int absPage = 1;
+		if(rowcount % pagesize == 0)
+			absPage = 0;
+		int pageCount = rowcount / pagesize + absPage;
+		int[] pages = new int[pageCount];
+		for(int i = 0; i <pageCount;i++) {
+			pages[i] = i+1;
+		}
+		
 		model.addAttribute("rankinfos",rankinfos);
 		model.addAttribute("members",members);
 		model.addAttribute("rankvisitbooks",rankvisitbooks);
 		model.addAttribute("rankvisitbooknumber",rankvisitbooknumber);
+		model.addAttribute("pagebooks",pagebooks);
+		model.addAttribute("pages",pages);
 		
 		return "rank/rankinfo_form";
+	}
+	
+	
+	
+	@RequestMapping(value="selectedPage",method=RequestMethod.GET)
+	public String selectedPage(Model model,@RequestParam int page, @RequestParam String id) {
+		RankInfoDao dao = sqlSession.getMapper(RankInfoDao.class);
+		
+		ArrayList<RankVisitbook> rankvisitbooks = dao.selectAll(id);
+		
+		int pagesize = 10;
+		int startrow = (page - 1) * pagesize;
+		int endrow = pagesize;
+		
+		rankvisitbooknumber.setStartrow(startrow);
+		rankvisitbooknumber.setEndrow(endrow);
+		ArrayList<RankVisitbook> pagebooks = dao.pageList(rankvisitbooknumber);
+		
+		int rowcount = rankvisitbooks.size();
+		
+		int absPage = 1;
+		if(rowcount % pagesize == 0)
+			absPage = 0;
+		int pageCount = rowcount / pagesize + absPage;
+		int[] pages = new int[pageCount];
+		for(int i= 0; i <pageCount; i++) {
+			pages[i] = i+1;
+		}
+		model.addAttribute("pagebooks",pagebooks);
+		model.addAttribute("pages",pages);
+		
+		System.out.println("선택한 페이지"+page);
+		System.out.println("id값 확인"+id);
+		
+		return "rank/rankinfo_form";
+	}
+	
+	
+	@RequestMapping(value = "/visitbookDel", method = RequestMethod.POST)
+	@ResponseBody
+	public String visitbookDel(Model model,@ModelAttribute RankVisitbook rankvisitbook,
+			@RequestParam int seq) {
+		RankInfoDao dao = sqlSession.getMapper(RankInfoDao.class);
+		
+		dao.deleteRow(seq);
+		
+		return "";
 	}
 	
 	@RequestMapping(value = "/visitBook", method = RequestMethod.POST)
@@ -125,26 +243,7 @@ public class RankController {
 		return "";
 	}
 	
-	@RequestMapping(value = "/rankTest", method = RequestMethod.GET)
-	public String rankTest(Model model,@RequestParam String id,@ModelAttribute Member member,
-			@ModelAttribute RankVisitbook rankvisitbook,@ModelAttribute RankVisitbookNumber rankvisitbooknumber ) {
-		MemberDAO mdao = sqlSession.getMapper(MemberDAO.class);
-		RankTestDao dao = sqlSession.getMapper(RankTestDao.class);
-		RankInfoDao idao = sqlSession.getMapper(RankInfoDao.class);
-		
-		Member members = mdao.selectOne(id);
-		ArrayList<RankVisitbook> rankvisitbooks = idao.selectAll(id); 
-		
-		int number = (int) rankvisitbooks.size();
-		rankvisitbooknumber.setNumber(number);
-		
-		
-		model.addAttribute("members",members);
-		model.addAttribute("rankvisitbooks",rankvisitbooks);
-		model.addAttribute("rankvisitbooknumber",rankvisitbooknumber);
-		
-		return "rank/rankinfo_form";
-	}
+	
 	
 	
 	
